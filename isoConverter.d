9727abb88f17905cd2c3d5771b1d3d708f64580f
Martijn van Iersel <mvaniersel@gmail.com>
2024-06-02 15:25:30 +0200

    Double-size isoConverter has fewer glitches. I can downscale the spritesheet manually

--- isoConverter.d
+++ isoConverter.d
@@ -19,37 +19,42 @@ Bitmap convertStack(Bitmap images, int num) {
 	int tileNum = images.w / images.h;
 
 	// create a new bitmap with extra margin
-	Bitmap result = Bitmap.create(tileWidth * 2 * num, tileHeight * 2);
+	Bitmap result = Bitmap.create(tileWidth * 4 * num, tileHeight * 4);
 	al_set_target_bitmap(result.ptr);
 	al_clear_to_color(al_map_rgba_f(0, 0, 0, 0));
 
-	Bitmap temp = Bitmap.create(tileWidth * 2, tileHeight * 2);
+	Bitmap temp = Bitmap.create(tileWidth * 4, tileHeight * 4);
 
 	float angle = 0.0f;
 	foreach(a; 0..num) {	
-		Bitmap dest = result.subBitmap(tileWidth * 2 * a, 0, tileWidth * 2, tileHeight * 2);
+		Bitmap dest = result.subBitmap(tileWidth * 4 * a, 0, tileWidth * 4, tileHeight * 4);
 
 		foreach(i; 0..tileNum) {
 			Bitmap tile = images.subBitmap(i * tileWidth, 0, tileWidth, tileHeight);
 			al_set_target_bitmap(temp.ptr);
 			al_clear_to_color(al_map_rgba_f(0, 0, 0, 0));
-			al_draw_rotated_bitmap(tile.ptr, tileWidth / 2, tileHeight / 2, tileWidth, tileHeight, angle * PI / 180.0, 0);
+
+			// rotate and make scale double size
+			al_draw_scaled_rotated_bitmap(tile.ptr, 
+				tileWidth / 2, tileHeight / 2, 
+				tileWidth * 2, tileHeight * 2, 
+				2.0f, 2.0f,
+				angle * PI / 180.0, 0);
 			
 			// now scale by 50% vertically with offset:
 			al_set_target_bitmap(dest.ptr);
 			al_draw_scaled_bitmap(temp.ptr, 
 				0, 0, 
-				tileWidth * 2, tileHeight * 2, 
-				0, tileNum - i, 
-				tileWidth * 2, tileHeight, 
+				tileWidth * 4, tileHeight * 4, 
+				0, (tileNum - i) * 2, 
+				tileWidth * 4, tileHeight * 2, 
 				0);
-
-			// Doesn't give the right effect...
-			// al_draw_scaled_rotated_bitmap(tile.ptr, 
-			// 	tileWidth / 2, tileHeight / 2, 
-			// 	tileWidth, tileHeight - i, 
-			// 	1.0f, 0.5f, 
-			// 	angle * PI / 180.0, 0);
+			al_draw_scaled_bitmap(temp.ptr, 
+				0, 0, 
+				tileWidth * 4, tileHeight * 4, 
+				0, (tileNum - i) * 2 - 1, 
+				tileWidth * 4, tileHeight * 2, 
+				0);
 
 			tile.destroy();
 		}
