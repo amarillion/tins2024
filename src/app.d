5ddfefabaa2c80a5ca0d74a9a445187c79ca52a5
Martijn van Iersel <martijn@Martijns-Mac-mini.fritz.box>
2024-07-06 22:04:50 +0200

    Fixes for Mac OS X Portability

--- src/app.d
+++ src/app.d
@@ -11,12 +11,52 @@ import helix.richtext;
 
 import dialog;
 import mainState;
+import std.stdio;
+
+import core.thread;
+
+extern (C) void rt_moduleTlsCtor();
+extern (C) void rt_moduleTlsDtor();
+
+// suggestion by SiegeLord https://discord.com/channels/993415281244393504/1013128461319159932/1259228267173515417
+// to solve problems with creating an app bundle on Mac OS X
+// Should eventually make its way to DAllegro...
+int al_run_allegro2(scope int delegate() user_main)
+{
+    __gshared int delegate() user_main2;
+    user_main2 = user_main;
+    extern(C) static int main_runner(int argc, char** argv)
+    {
+        version(OSX)
+        {
+            thread_attachThis();
+            rt_moduleTlsCtor();
+        }
+
+        auto main_ret = user_main2();
+
+        version(OSX)
+        {
+            thread_detachThis();
+            rt_moduleTlsDtor();
+        }
+
+        return main_ret;
+    }
+
+    return al_run_main(0, null, &main_runner);
+}
 
 void main(string[] args)
 {
-	al_run_allegro(
+	al_run_allegro2(
 	{
 		al_init();
+
+		// look up resources directory for Mac OS X,
+		// after: https://github.com/liballeg/allegro_wiki/wiki/Creating-macOS-bundles
+		al_change_directory(al_path_cstr(al_get_standard_path(ALLEGRO_RESOURCES_PATH), '/'));
+ 
 		auto mainloop = new MainLoop(MainConfig.of
 			.appName("tins24")
 			.targetFps(60)
